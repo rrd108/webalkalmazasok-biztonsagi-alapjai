45723bf0b0ea36576b6358efe9ec0ced11906b97
rrd <rrd@1108.cc>
2021-11-28 17:25:09 +0100

    fixes added

--- db.php
+++ db.php
@@ -9,26 +9,30 @@ $stmt = $pdo->query('SELECT * FROM products, categories
 
 if (isset($_GET['category'])) {
     // Nem biztonságos mód
-    $query = 'SELECT * FROM products, categories 
+    /*$query = 'SELECT * FROM products, categories 
     WHERE categories.id = products.category_id 
     AND categories.category = "' . $_GET['category'] . '"
     ORDER BY rand()';
-    $stmt = $pdo->query($query);
+    $stmt = $pdo->query($query);*/
 
     // Biztonságos mód
-    /*
     $stmt = $pdo->prepare('SELECT * FROM products, categories 
     WHERE categories.id = products.category_id 
     AND categories.category = ?
     ORDER BY rand()');
     $stmt->execute([$_GET['category']]);
-    */
 }
 
 if (isset($_GET['order'])) {
+    // Nem biztonságos mód
     $stmt = $pdo->query('SELECT * FROM products, categories 
         WHERE categories.id = products.category_id 
         ORDER BY ' . $_GET['order']);
+    // Biztonságos mód
+    /*$stmt = $pdo->prepare('SELECT * FROM products, categories 
+    WHERE categories.id = products.category_id 
+    ORDER BY ?');
+    $stmt->execute([$_GET['order']]);*/
 }
 
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
--- login.php
+++ login.php
@@ -1,15 +1,14 @@
 <?php
 
 // Nem biztonságos mód
-$query = 'SELECT * FROM users  WHERE email = "' . $_POST['email'] . '" AND pass = "' . $_POST['password'] . '"';
-// SELECT * FROM users  WHERE email = "senki@sehol.se" AND pass = "123456789"
-// SELECT * FROM users  WHERE email = "" OR 1=1 -- rrd AND pass = "123456789"
-$stmt = $pdo->query($query);
+//$query = 'SELECT * FROM users  WHERE email = "' . $_POST['email'] . '" AND pass = "' . md5($_POST['password']) . '"';
+/* SELECT * FROM users  WHERE email = "senki@sehol.se" AND pass = "123456789"
+SELECT * FROM users  WHERE email = "" OR 1=1 -- rrd AND pass = "123456789"*/
+//$stmt = $pdo->query($query);
 
-/* Biztonságos mód
+// Biztonságos mód
 $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND pass = ?');
-$stmt->execute([$_POST['email'], $_POST['password']]);
-*/
+$stmt->execute([$_POST['email'], md5($_POST['password'])]);
 
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 if ($user) {
